dbccfa9cc0ef1094c6a208510f159c73ecaf7df0
varunkmr19 <varun-kumar@live.com>
2021-01-07 06:20:58 +0530

    add registration and logout

--- homework/urls.py
+++ homework/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.LoginView.as_view(), name='login_view'),
+    path('logout', views.LogoutView.as_view(), name='logout'),
     path('register', views.RegistrationView.as_view(), name='register')
 ]
--- homework/views.py
+++ homework/views.py
@@ -1,3 +1,5 @@
+from homework.models import Student
+from django.db import IntegrityError
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
@@ -31,6 +33,43 @@ class LoginView(View):
         return render(request, 'homework/login.html')
 
 
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse('index'))
+
+
 class RegistrationView(View):
+    def post(self, request):
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        group_id = request.POST['group_id']
+
+        # Ensure password matches confirmation
+        password = request.POST['password']
+        confirmation = request.POST['confirm-password']
+        if password != confirmation:
+            return render(request, 'homework/register.html', {
+                'error': 'Passwords must match.'
+            })
+
+        # Attempt to create new user
+        try:
+            user = Student.objects.create_user(
+                username, email, password,
+                first_name=first_name,
+                last_name=last_name,
+                group_id=group_id
+            )
+            user.save()
+        except IntegrityError:
+            return render(request, 'homework/register.html', {
+                'error': 'Username already taken.'
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse('index'))
+
     def get(self, request):
         return render(request, 'homework/register.html')
